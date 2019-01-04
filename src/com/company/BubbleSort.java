f93cb64f2715c0d68914ebd8c58e526af90e5c20
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2019-01-04 08:40:54 -0500

    Initial commit

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class BubbleSort {
+    public static void Swap(int[] arr, int i, int j){
+        int t= arr[i];
+        arr[j]= arr[i];
+        arr[j]=t;
+    }
+    public static void bubbleSort(int[] arr){
+    int n=1;
+        while (n > 0) {
+            for(int x=0,x<arr.length,x+1)
+        }
+}
---
+++ src/com/company/BubbleSortRunner.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class BubbleSortRunner {
+    public static void main(String[] args)
+    {
+        int[] testArr={8,6,7,5,3,0,9,10,1,2,3};
+        System.out.print("Before:");
+        for(int num:testArr)
+            System.out.print(num+"");
+        System.out.println();
+        BubbleSort.bubbleSort(testArr);
+        System.out.print("After:");
+        for(int num:testArr)
+            System.out.print(num+"");
+        System.out.println();
+    }
+}
+
