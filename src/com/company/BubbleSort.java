39b2a1924a6d2cfe1513be5f233fa4452dcd5681
yhuang7593 <yhuang7593@bths.edu>
2019-01-10 01:20:10 -0500

    new

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,14 +1,14 @@
 package com.company;
 
 public class BubbleSort {
-    public static void Swap(int[] arr, int i, int j){
-        int t= arr[i];
-        arr[j]= arr[i];
-        arr[j]=t;
-    }
-    public static void bubbleSort(int[] arr){
-    int n=1;
-        while (n > 0) {
-            for(int x=0,x<arr.length,x+1)
+    public static void bubbleSort(int[] Arr) {
+        int temp;
+            for (int i = 0; i < Arr.length-1; i++) {
+                if (Arr[i] > Arr[i + 1]) {
+                    temp = Arr[i + 1];
+                    Arr[i + 1] = Arr[i];
+                    Arr[i]=temp;
+                }
+            }
         }
-}
+    }
--- src/com/company/BubbleSortRunner.java
+++ src/com/company/BubbleSortRunner.java
@@ -3,7 +3,7 @@ package com.company;
 public class BubbleSortRunner {
     public static void main(String[] args)
     {
-        int[] testArr={8,6,7,5,3,0,9,10,1,2,3};
+        int[] testArr={2,3,9,6,8};
         System.out.print("Before:");
         for(int num:testArr)
             System.out.print(num+"");
