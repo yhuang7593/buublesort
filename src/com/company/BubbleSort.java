05f03ab3eb8cc6cbb9276b283b383e567a7fd213
yhuang7593 <yhuang7593@bths.edu>
2019-01-11 08:25:28 -0500

    Updated

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -3,12 +3,15 @@ package com.company;
 public class BubbleSort {
     public static void bubbleSort(int[] Arr) {
         int temp;
-            for (int i = 0; i < Arr.length-1; i++) {
+        int j;
+        for (j = 0; j < Arr.length - 1; j++) {
+            for (int i = 0; i < Arr.length - 1; i++) {
                 if (Arr[i] > Arr[i + 1]) {
                     temp = Arr[i + 1];
                     Arr[i + 1] = Arr[i];
-                    Arr[i]=temp;
+                    Arr[i] = temp;
                 }
             }
         }
     }
+}
